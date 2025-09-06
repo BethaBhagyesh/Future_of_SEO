983d9602eb00af7232d131fa81efc126032b5e76
Bhagyesh Betha <bethabhagyesh@gmail.com>
2025-09-06 14:19:10 +0530

    Renamed Code.txt to Code.tsx

---
+++ Code.tsx
@@ -0,0 +1,14 @@
+// Your existing content from Code.txt goes here
+
+// Example React TypeScript Component
+import React from 'react';
+
+const MyComponent: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+};
+
+export default MyComponent;
